2f644f7622816fd48d184c330ad6908f4c157083
Moises Vilas Boas <moisesboas@gmail.com>
2024-05-26 13:10:54 -0300

    feat: object-types creating the contract and making be equals in contract

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import { ApolloServer, gql } from 'apollo-server';
 
 const server = new ApolloServer({
   typeDefs: gql`
-    type Query {}
+    type Query {
+      user: User
+    }
+
+    type User {
+      id: ID!
+      userName: String!
+    }
   `,
 
   resolvers: {
-    Query: {},
+    Query: {
+      user: () => {
+        return {
+          id: 'Aabcd-12345',
+          userName: 'devMoisa',
+        };
+      },
+    },
   },
 });
 
